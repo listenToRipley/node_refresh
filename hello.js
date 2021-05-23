edf266d6070c722e89d68badc3dae022e1379e58
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-05-23 16:56:26 -0500

    server coming across correctly

--- hello.js
+++ hello.js
@@ -1,5 +1,10 @@
 var http = require('http');
 
+//function all
+function serverCallback(req, res) {
+  res.writeHead(200, {'Content-Type':'text/plain'});
+  res.end("Hello World");
+}
 
-
-http.createServer(serverCallbacl).listen(8080);
+//create the sever
+http.createServer(serverCallback).listen(8080);
