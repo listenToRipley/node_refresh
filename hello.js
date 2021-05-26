29864201186d84c26a69aac44c3320213469b8d3
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-05-25 20:33:15 -0500

    formatting momemnt content

--- hello.js
+++ hello.js
@@ -1,10 +1,11 @@
 var http = require('http');
 //start server by calling node
+var moment = require('moment');
 
 //function all
 function serverCallback(req, res) {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end("Hello " + process.argv[2]); //perm to call in the start message
+  res.end("Hello " + process.argv[2] + '. ' + moment().format('LLLL') + " now."); //perm to call in the start message
 }
 
 //create the sever
