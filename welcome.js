0e3546a16c25d8dd7be3dd03051eecce364deac7
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-05-25 21:27:13 -0500

    subtraction off still

--- welcome.js
+++ welcome.js
@@ -3,36 +3,11 @@ var http = require('http');
 var moment = require('moment');
 
 function callbackServer(req, res) {
-  var birth_month = moment('07', 'MM')
-  var birth_day = moment('18', 'DD')
-  var month_down = moment().diff(birth_month)
-  var days_down = moment().diff(birth_day)
-  var count_down = ''
+  var month_down = moment('07', 'MM').fromNow();
+  var days_down = moment('18', 'DD').fromNow();
 
-  //test if we are in the month, just count down days
-  //if the month is greater than 1, it should be months
-  //if month is equal to 1, it should be month
-
-  //if both month and day equals to 0, today is your birthday 
-  //if days greater than 1, it should be days
-  //if day is equal to 1, it should be day
-
-  if(month_down === 0 && days_down === 0) {
-    count_down = ' TODAY IS YOUR BIRTHDAY!'
-  } 
-
-  if(month_down === 0) {
-    count_down = `${days_down} days until your birthday! `
-  } else if (month_down === 1) {
-      
-    if(days_down === 1) {
-        count_down = `${month_down} month and ${days_down} day until your birthday! `
-      } else {
-        count_down = `${month_down} month and ${days_down} days until your birthday! `
-      }
-  } else {
-    count_down = `${month_down} months and ${days_down} days until your birthday! `
-  }
+  console.log(' months ', month_down)
+  console.log( ' days ' , days_down)
 
   res.writeHead(200, {
     'Content-Type': 'text/plain'
@@ -42,7 +17,7 @@ function callbackServer(req, res) {
     ' today is : \n'
     + moment().format('LLL') +
     ' \n ' + 
-    count_down
+    month_down + ' ' + days_down
   )
 }
 
