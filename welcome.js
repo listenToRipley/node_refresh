f28d23f829bb980906d46c275c65cd0a20fc0715
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-05-30 15:41:02 -0500

    diff makes more sense and functions better

--- welcome.js
+++ welcome.js
@@ -3,12 +3,22 @@ var http = require('http');
 var moment = require('moment');
 
 function callbackServer(req, res) {
-  var month_birth = moment('07', 'MM');
+  var month_birth = moment('010', 'MM');
   var days_birth =  moment('18', 'DD');
 
-  var month_down = month_birth.diff(moment(), 'months')
+  var month_down = Math.abs(parseInt(month_birth.diff(moment(), 'months')))
 
-  var days_down = Math.abs(parseInt(days_birth.diff(moment(), 'days')))
+  var days_down = moment().diff(days_birth, 'days')
+  
+  var time = ''
+
+  if(month_down > 0) {
+    time = `It is ${days_down} days until your birthday`
+  } else if (month_down === 1) {
+    time = `It is a month and ${days_down} days until your birthday`
+  } else {
+    time = `It is ${month_down} months and ${days_down} days until your birthday`
+  }
 
   res.writeHead(200, {
     'Content-Type': 'text/plain'
@@ -17,9 +27,7 @@ function callbackServer(req, res) {
     'Welcome to my headspace! \n'  +
     ' today is : \n'
     + moment().format('LLL') +
-    ' \n ' + 
-    ' It is ' +
-    month_down + ' months and ' + days_down + ' days until your birthday!'
+    ' \n ' + time
   )
 }
 
